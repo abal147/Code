e1c5d479627cc0541c65c7be1a11df87c3092d1e
abal147 <aaron.balsara@gmail.com>
2014-08-31 19:44:46 +1000

    Problem 272: Tex Quotes

---
+++ UVA/272.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+int main() {
+	bool seen = false;
+	int c;
+	while ((c = getchar()) > 0) {
+		if (c < 0) break;
+		if (c == '"') {
+			if (!seen) {
+				putchar('`');
+				putchar('`');
+			} else {
+				putchar('\'');
+				putchar('\'');
+			}
+			seen = !seen;
+		} else {
+			putchar(c);
+		}
+	}
+	return 0;
+}
